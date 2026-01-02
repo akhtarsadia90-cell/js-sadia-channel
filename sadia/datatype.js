ff6777cfe6f9e64f195a1400a2598a413285d1c0
akhtarsadia90-cell <akhtarsadia90@gmail.com>
2026-01-02 05:41:38 +0000

    datatype.js

---
+++ sadia/datatype.js
@@ -0,0 +1,27 @@
+"use strict";    // treat all js code as a new version
+
+// alert (3 + 3) we r using node js not browser
+
+console.log(3+4); console.log("sadia");
+
+let name = "sadia"
+let age = 23
+let isLoggedIn = false
+let state;
+
+// number => 2 to power 53
+// bigint
+//string => ""
+//boolean => trus/false
+//null => standalon value
+// unndefined =>
+// symbol => unique
+
+
+//object
+console.log(typeof null); //obj
+console.log(typeof "sadia"); // string
+
+
+
+
