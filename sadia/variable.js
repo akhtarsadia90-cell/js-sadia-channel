f12b3ee1781027189f62260b0ba0c29d2a76f196
akhtarsadia90-cell <akhtarsadia90@gmail.com>
2026-01-02 05:40:21 +0000

    variable.js

---
+++ sadia/variable.js
@@ -0,0 +1,20 @@
+const accountId = 144553
+let accountEmail = "akhtarsadia90@gmail.com"
+var accountPassword = "12345"
+accountCity = "Muridke"
+accountState;
+
+// accountId = 2 not allowed due to const because const remains the same
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+
+accountEmail = "hyysgfgh"
+accountPassword = "786786"
+accountCity = "Pakistan"
+
+// prefer not use of var because issue of block scope & functional scope
+
+console.log(accountId, accountEmail, accountPassword, accountCity, accountState);
