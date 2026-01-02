b8192f1e1a1b5f061f4da6ed54d6d2f2f4f6e367
akhtarsadia90-cell <akhtarsadia90@gmail.com>
2026-01-02 10:41:32 +0000

    datatype_summary.js

---
+++ sadia/datatype_summary.js
@@ -0,0 +1,35 @@
+// Primitive Datatype (one value store, obj not store)
+   
+// 7 Types : string, number, boolean, null, undefined, symbol(unique), bigint
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol ('123')
+const anotherId = Symbol ('123')
+
+console.log(id === anotherId);
+const bigNumber =6677644567889n
+
+
+// Reference (non primitive)
+//non premitive ka return type obj hi ata h 
+// Arrays, objects, functions
+
+const heros =[ "sadia", "malik", "sadi"]
+let myObj ={
+
+    name:"sadia",
+    age:23,
+
+}
+
+const myFunction = function () {
+    console.log("sadia maliks");
+}
+
+console.log(typeof outsideTemp);
